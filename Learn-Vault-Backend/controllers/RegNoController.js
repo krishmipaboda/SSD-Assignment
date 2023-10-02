e1b1890c6d2e199a3f6fd973dfaba5efe74cf4d7
ahamedazmie5 <ahamedazmie5@gmail.com>
2023-10-03 00:25:05 +0530

    Use of Externally-Controlled Format String (Fix No 1)

--- Learn-Vault-Backend/controllers/RegNoController.js
+++ Learn-Vault-Backend/controllers/RegNoController.js
@@ -1,50 +1,47 @@
 const express = require('express');
-const mongoose  = require ('mongoose');
-const {title} = require ('process');
+const mongoose = require('mongoose');
 const RegNoModel = require('../models/RegNo');
 
-const createRegNO = async (res,req) =>{
-    console.log('blogs');
-    const Regdata = req.body;
-    console.log(Regdata,'Course is added');
+const createRegNo = async (req, res) => {
+  console.log('Creating RegNo');
+  const Regdata = req.body;
 
+  try {
     const newRegNo = new RegNoModel(Regdata);
-    console.log('saved data', newRegNo);
-    try {
-      await newRegNo.save();
-  
-      res.status(200).json({ newRegNo });
-    } catch (error) {
-      res.status(409).json({ message: error.message });
-    }
+    await newRegNo.save();
+    console.log('Saved data:', JSON.stringify(newRegNo)); // Log data safely
+
+    res.status(200).json({ newRegNo });
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
 };
 
 const getRegNoById = async (req, res) => {
-    const { id } = req.params;
-    console.log(id, 'RegIstration Number');
-  
-    try {
-      const RegNo = await RegNoModel.findById(id);
-      console.log(RegNo, 'Get RegIstration By id');
-  
-      res.status(200).json(RegNo);
-    } catch (error) {
-      res.status(404).json({ message: error.message });
-    }
-  };
+  const { id } = req.params;
+  console.log('Fetching RegNo by ID:', id);
 
-  const getAllRegNo = async (req, res) => {
-    console.log('All RegNo');
-  
-    try {
-      const AllRegNo = await RegNoModel.find();
-      console.log(AllRegNo);
-  
-      res.status(200).json(AllRegNo);
-    } catch (error) {
-      res.status(404).json({ message: error.message });
-    }
-  };
+  try {
+    const RegNo = await RegNoModel.findById(id);
+    console.log('Fetched RegNo:', JSON.stringify(RegNo)); // Log data safely
 
+    res.status(200).json(RegNo);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 
-  module.exports = {getAllRegNo,getRegNoById,createRegNO}
+const getAllRegNo = async (req, res) => {
+  console.log('Fetching all RegNo');
+
+  try {
+    const allRegNo = await RegNoModel.find();
+    console.log('Fetched all RegNo:', JSON.stringify(allRegNo)); // Log data safely
+
+    res.status(200).json(allRegNo);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+module.exports = { getAllRegNo, getRegNoById, createRegNo };
